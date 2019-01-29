e0cbdcbabc8939b77c1731c624e11817185c4bc0
Salatiel Osorno <sosorno@marathongroup.mx>
2019-01-28 18:02:08 -0600

    My resume.

---
+++ README.md
@@ -0,0 +1,15 @@
+# My resume
+
+This is my resume. I have made it just for fun, but here I have applied knowledge about HTML, CSS, JS, and packaging using "webpack"
+
+## Sections
+
+* Welcome page
+* About me
+* Skills
+* Experience
+* Get in touch
+
+## License
+Feel free getting code to learn, fun, or whatever you want. Just change info.
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin  = require('clean-webpack-plugin');
+const CopyWebpackPlugin  = require('copy-webpack-plugin');
+
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/assets/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './docs'
+    },
+    output: {
+        filename:  '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'docs')
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin(),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jpg|png|jpeg|svg)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        //options: {
+                            // you can specify a publicPath here
+                            // by default it use publicPath in webpackOptions.output
+                            //publicPath: '../'
+                        //}
+                    },
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['docs']),
+        new HtmlWebpackPlugin({
+            title: 'Resume',
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+        new CopyWebpackPlugin([{
+            from: 'src/assets', to: 'assets'
+        }]),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css",
+            chunkFilename: "[id].[contenthash].css"
+        })
+    ]
+}
