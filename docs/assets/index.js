c5b08464f39e5d65861e7adb6155985c01ea83a9
Salatiel Osorno <salatiel.linares@gmail.com>
2019-07-05 12:45:24 -0500

    Se cambia estructura y diseño. Se agrega sección Courses.

--- docs/assets/index.js
+++ docs/assets/index.js
@@ -1,42 +1,21 @@
 import './styles.css'
 
-function addEventKnowme(){
-    let barmenu = document.body.querySelector('#knowme');
-    barmenu.addEventListener('click', (e)=> {
-        showMenu(e.currentTarget);
-    });
-}
 function addEventsButtons(){
-    let links = document.body.querySelectorAll('.nav__item');
-
+    let links = document.body.querySelectorAll('.nav__link');
     links.forEach((e,k)=>{
         e.addEventListener('click', ()=>{
-            hide(links);
+            hide(links)
             let link = document.body.querySelector('#'+e.dataset.link);
             link.style.display = 'block';
-            link.classList.remove('slideRight');
-            link.classList.add('wrap__content--margin');
+
+            document.getElementById("wrap__content").scrollTop = 0
         });
     })
-}
-function showMenu(target){
-    let nav = document.body.querySelector(".wrap__content");
-    nav.classList.add('slideRight');
-    target.style.visibility = 'hidden';
 }
 function hide(links){
     links.forEach((e,k)=>{
         let link = document.body.querySelector('#'+e.dataset.link);
         link.style.display = 'none';
-        document.body.querySelector('#home').style.display = 'none';
     })
 }
-
-setTimeout(() => {
-    let button = document.body.querySelector('#knowme');
-    button.style.visibility = 'visible';
-    button.style.opacity = 1;
-}, 2000);
-
-addEventKnowme();
 addEventsButtons();
