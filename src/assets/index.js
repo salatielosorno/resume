2dd4643be6a9d5bafb74618d87d82d017dc7f022
Salatiel Osorno <salatiel.linares@gmail.com>
2019-08-13 12:11:16 -0500

    Se agregan estilos para soporte en moviles.

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,18 +4,77 @@ function addEventsButtons(){
     let links = document.body.querySelectorAll('.nav__link');
     links.forEach((e,k)=>{
         e.addEventListener('click', ()=>{
-            hide(links)
+            hideSections(links)
             let link = document.body.querySelector('#'+e.dataset.link);
-            link.style.display = 'block';
+            
+            if((link === null || link === undefined) && window.innerWidth <= 540)
+                link = document.body.querySelector('.'+e.dataset.link);
 
-            document.getElementById("wrap__content").scrollTop = 0
+            if(link != null || link != undefined){
+                link.style.display = 'block';
+            }
+            setLinkActive(e.dataset.link)
+            document.getElementById("wrap__content").scrollTop = 0;
+            document.documentElement.scrollTop = 0;
         });
     })
 }
-function hide(links){
+function setLinkActive(id) {
+    let links = document.body.querySelectorAll('[data-link="' + id + '"]');
     links.forEach((e,k)=>{
-        let link = document.body.querySelector('#'+e.dataset.link);
-        link.style.display = 'none';
+        e.classList.add('nav__link-active')
     })
 }
+function unsetLinkActive(id) {
+    let links = document.body.querySelectorAll('[data-link="' + id + '"]');
+    links.forEach((e,k)=>{
+        e.classList.remove('nav__link-active')
+    })
+}
+function hideSections(links){
+    links.forEach((e,k)=>{
+        let link = document.body.querySelector('#'+e.dataset.link);
+        
+        if((link === null || link === undefined) && window.innerWidth <= 540)
+            link = document.body.querySelector('.'+e.dataset.link);
+        
+        if(link != null || link != undefined)
+            link.style.display = 'none';
+        unsetLinkActive(e.dataset.link)
+    })
+}
+function hideHomeMobil(){
+    let homeMobil = document.querySelector('#profile');
+    homeMobil.style.display = 'none';
+}
+function showAboutMe(){
+    let homeMobil = document.querySelector('#about');
+    homeMobil.style.display = 'block';
+}
+function load() {
+    if(window.innerWidth <= 540){
+        let profile = document.querySelector("#profile");
+        let about = document.querySelector("#about");
+        profile.style.height = (window.innerHeight - 73) + "px";
+        profile.style.display = 'block';
+        about.style.display = 'none';
+    }
+}
+function resize(){
+    let linksActive = document.querySelectorAll('.nav__link-active');
+
+    linksActive.forEach((e,k)=>{
+        let sectionActive = e.dataset.link;
+
+        if(sectionActive == 'profile'){
+            document.querySelector("#profile").style.height = (window.innerHeight - 73) + "px";
+            hideHomeMobil();
+            showAboutMe();
+        }
+    })
+}
+
 addEventsButtons();
+window.onresize = () => { resize() }
+window.onload = () => { load() }
+window.addEventListener('orientationchange', resize);
